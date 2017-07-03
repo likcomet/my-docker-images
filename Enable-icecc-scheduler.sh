e293737699da7d8e5eb9309895fa2153aae248e4
likcomet <likcomet@gmail.com>
2017-07-03 18:07:16 +0900

    icecream distributed compiler 2017/07/03

--- Enable-icecc-scheduler.sh
+++ Enable-icecc-scheduler.sh
@@ -2,7 +2,7 @@
 set -e
 case $ICECREAM_SCHEDULER in
 	yes) icecc-scheduler -d -n neople -l /var/log/icecc-scheduler
-             iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 35 -n neople -l /var/log/icecc.log && tail -f /var/log/icecc.log
+             iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 20 -n neople -l /var/log/icecc.log && tail -f /var/log/icecc.log
 	;;
 	no)  iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 35 -n neople -l /var/log/icecc.log && tail -f /var/log/icecc.log
 	;;
