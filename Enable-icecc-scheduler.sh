e4983138806abc89fa065a321a0673619715379b
likcomet <likcomet@gmail.com>
2017-07-03 13:24:09 +0900

    icecream distributed compiler

--- Enable-icecc-scheduler.sh
+++ Enable-icecc-scheduler.sh
@@ -2,9 +2,9 @@
 set -e
 case $ICECREAM_SCHEDULER in
 	yes) icecc-scheduler -d -l /var/log/icecc-scheduler
-	     echo "/usr/local/sbin/icecc-scheduler -d -l /var/log/icecc-scheduler --nice 5 ICECREAM_SCHEDULER = ${ICECREAM_SCHEDULER}"
+	     echo "/usr/local/sbin/icecc-scheduler -d -l /var/log/icecc-scheduler --nice -20 ICECREAM_SCHEDULER = ${ICECREAM_SCHEDULER}"
 	;;
 	*)   echo "Null"
 	;;
 esac
-iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 30 -l /var/log/icecc.log && tail -f /var/log/icecc.log
+iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 35 -l /var/log/icecc.log && tail -f /var/log/icecc.log
